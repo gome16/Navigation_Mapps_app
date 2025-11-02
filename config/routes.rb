5708c15005ebfaf34b7d253ea02c17b4576c0937
Ubuntu <ubuntu@ip-172-31-4-103.ap-northeast-1.compute.internal>
2025-11-02 16:06:27 +0000

    [Add] ユーザーのdevise導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  devise_for :users,skip: [:passwords],
+    controllers: {
+      sessions: 'public/users/sessions',
+      registrations: 'public/users/registrations',
+    }
+
+  namespace :public do
+    resources :users, only: [:show]
+  end
+
   root to: "homes#top"
   get 'homes/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
