598c81992f25422d88b7c784b46cd0115fb7c6af
Ubuntu <ubuntu@ip-172-31-4-103.ap-northeast-1.compute.internal>
2025-11-03 12:07:54 +0000

    [Add] ユーザー新規投稿機能実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  # プロフィール画像とデフォルト未設定の場合のデフォルト画像設定
+  def profile_image_for(user, size: [200, 200])
+    if user.profile_image.attached?
+      image_tag user.profile_image.variant(resize_to_limit: size)
+    else
+      image_tag 'default_profile.jpg', size: "#{size[0]}x#{size[1]}"
+    end
+  end
 end
