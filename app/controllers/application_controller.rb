ca72d96b22b0d174e4ceedb3930325302fefd0ca
Ubuntu <ubuntu@ip-172-31-4-103.ap-northeast-1.compute.internal>
2025-12-16 21:21:45 +0000

    [chore] 削除済みの投稿アクセス時の処理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
 
   # サインイン後の遷移先指定
   def after_sign_in_path_for(resource)
@@ -16,5 +18,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :profile, :profile_image])
     devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :profile, :profile_image])
+  end
+
+  def record_not_found
+    redirect_to posts_path, alert: "投稿は削除済みです"
   end
 end
